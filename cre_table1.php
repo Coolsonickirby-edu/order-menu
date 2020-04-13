9079db511f9e1789fe5758c3193f4989baa07cec
Ali Hussain <alihussain2001@gmail.com>
2020-04-13 15:51:18 -0400

    fixed

--- cre_table1.php
+++ cre_table1.php
@@ -8,7 +8,14 @@ $database = $_SQL_DATABASE;
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE orders ( order_idint(10) not null auto_increment, payment_idint(10) not null,menuitemvarchar(40),quantityint,primary key (order_id) )";
+    $sql = "CREATE TABLE orders (
+                    order_id int(10) not null auto_increment,
+                    payment_id int(10) not null,
+                    menuitem varchar(40),
+                    quantity int,
+                    primary key (order_id)
+                )
+            ";
     $conn->exec($sql);
     echo "Table created successfully";
 } catch (PDOException $e) {
--- cre_table2.php
+++ cre_table2.php
@@ -8,7 +8,26 @@ $database = $_SQL_DATABASE;
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE orders ( order_idint(10) not null auto_increment, payment_idint(10) not null,menuitemvarchar(40),quantityint,primary key (order_id) )";
+    $sql = "CREATE TABLE payment (
+                    payment_id int(10) not null auto_increment,
+                    firstname varchar(30),
+                    lastname varchar(30),
+                    phone1 varchar(3),
+                    phone2 varchar(3),
+                    phone3 varchar(4),
+                    email varchar(35),
+                    cardtype varchar(20),
+                    cardnumber varchar(20),
+                    zipcode varchar(5),
+                    subtotal decimal(6, 2),
+                    taxes decimal(6, 2),
+                    discount decimal(6, 2),
+                    total decimal(6, 2),
+                    couponcode varchar(5),
+                    orderdate datetime,
+                    primary key (payment_id)
+                )
+            ";
     $conn->exec($sql);
     echo "Table created successfully";
 } catch (PDOException $e) {
