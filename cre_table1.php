ab0913c8a46ed7686cdb8c1228eb47f25590a223
Ali Hussain <alihussain2001@gmail.com>
2020-04-13 15:23:31 -0400

    fixed

--- cre_table1.php
+++ cre_table1.php
@@ -1,31 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include("config.php");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-        include("config.php");
-
-        $servername = "localhost";
-        $username = $_SQL_USERNAME;
-        $password = $_SQL_PASSWORD;
-        $database = $_SQL_DATABASE;
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "CREATE TABLE orders ( order_idint(10) not null auto_increment, payment_idint(10) not null,menuitemvarchar(40),quantityint,primary key (order_id) )";
-            $conn->exec($sql);
-            echo "Table created successfully";
-        } catch (PDOException $e) {
-            echo $sql . "<br>" . $e->getMessage();
-        }
-        $conn = null; 
-    ?>
-</body>
-
-</html>
+$servername = $_SQL_ADDRESS;
+$username = $_SQL_USERNAME;
+$password = $_SQL_PASSWORD;
+$database = $_SQL_DATABASE;
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE orders ( order_idint(10) not null auto_increment, payment_idint(10) not null,menuitemvarchar(40),quantityint,primary key (order_id) )";
+    $conn->exec($sql);
+    echo "Table created successfully";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn = null;
--- cre_table2.php
+++ cre_table2.php
@@ -1,36 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include("config.php");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-    include("config.php");
-
-    $servername = "localhost";
-    $username = $_SQL_USERNAME;
-    $password = $_SQL_PASSWORD;
-    $database = $_SQL_DATABASE;
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "CREATE TABLE payment ( payment_idint(10) 
-                not null auto_increment, firstnamevarchar(30),lastnamevarchar(30),phone1varchar(3),
-                         phone2varchar(3),phone3varchar(4),emailvarchar(35),cardtypevarchar(20), cardnumbervarchar(20), 
-                         zipcodevarchar(5),subtotaldecimal(6,2),taxesdecimal(6,2),discountdecimal(6,2),totaldecimal(6,2),
-                         couponcodevarchar(5),orderdatedatetime,
-                primary key (payment_id) )";
-        $conn->exec($sql);
-        echo "Table created successfully";
-    } catch (PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
-    }
-    $conn = null;
-    ?>
-</body>
-
-</html>
+$servername = $_SQL_ADDRESS;
+$username = $_SQL_USERNAME;
+$password = $_SQL_PASSWORD;
+$database = $_SQL_DATABASE;
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE orders ( order_idint(10) not null auto_increment, payment_idint(10) not null,menuitemvarchar(40),quantityint,primary key (order_id) )";
+    $conn->exec($sql);
+    echo "Table created successfully";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn = null;
