a3cc95bc113a0f63660c0a5800d456bb404e02c5
Ali Hussain <alihussain2001@gmail.com>
2020-04-13 15:16:08 -0400

    Tmp Create Tabels

---
+++ cre_table1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        include("config.php");
+
+        $servername = "http://cislinux.hfcc.edu/~ahussain17";
+        $username = $_SQL_USERNAME;
+        $password = $_SQL_PASSWORD;
+        $database = $_SQL_DATABASE;
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "CREATE TABLE orders ( order_idint(10) not null auto_increment, payment_idint(10) not null,menuitemvarchar(40),quantityint,primary key (order_id) )";
+            $conn->exec($sql);
+            echo "Table created successfully";
+        } catch (PDOException $e) {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+        $conn = null; 
+    ?>
+</body>
+
+</html>
